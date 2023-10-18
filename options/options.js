0684ccc3cc900de98f049207c8ef5c3665167abe
da <Daolund@gmail.com>
2023-10-18 14:19:04 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "daveao20231012-43900",
-  url: "https://daveao20231012-43900.botics.co",
-  api: "https://daveao20231012-43900.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "daveao20231012-43900",
+  "url": "https://daveao20231012-43900.botics.co",
+  "api": "https://daveao20231012-43900.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
